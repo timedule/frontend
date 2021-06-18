955339a76157fc870221cb3814b68fb7f3ae34fe
Laddge <dev.laddge@gmail.com>
2021-06-18 21:07:33 +0900

    [feat] PWAに対応

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './registerServiceWorker'
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
