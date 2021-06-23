8018aeeddc7b161a025c21f96611382098a93a65
Laddge <dev.laddge@gmail.com>
2021-06-23 06:54:28 +0000

    [feat] 時間割のタイトル、作者、更新日を表示するようにした

--- src/components/View.vue
+++ src/components/View.vue
@@ -4,7 +4,15 @@
             <vue-loading class="my-3" type="spin" color="#007bff" :size="{width: '50px', height: '50px'}"></vue-loading>
         </div>
         <div v-if="response">
-            {{ this.response }}
+            <div class="p-3 border-bottom border-2">
+                <span class="h2">
+                    {{ this.response.title }}
+                </span>
+                <span>
+                    {{ this.response.owner }}
+                    {{ this.response.ua_formatted }}
+                </span>
+            </div>
         </div>
         <div v-if="error" class="alert alert-danger" role="alert">
             エラーが発生しました。もう一度お試しください。
@@ -25,6 +33,8 @@ export default {
       .get('https://timedule.herokuapp.com/table/' + this.$route.params.id)
       .then((response) => {
         this.response = response.data;
+        let ua = new Date(this.response.updated_at);
+        this.response.ua_formatted = String(ua.getFullYear()).slice(-2) + '/' + String(ua.getMonth() + 101).slice(-2) + '/' + String(ua.getDate() + 100).slice(-2);
       })
       .catch((reason) => {
         if (reason.response.status == 404) {
