fae2a7a30cd5d1395012a17df512a8465fda218c
Laddge <dev.laddge@gmail.com>
2021-06-24 05:51:48 +0000

    [feat] 時間割を整形して出力

--- src/components/View-table.vue
+++ src/components/View-table.vue
@@ -1,6 +1,23 @@
 <template>
     <div>
-    {{ arg }}
+        <div class="text-center my-3 h5 fw-normal">
+            <span class="border border-2 border-primary rounded py-1 px-2">
+                &lt; {{ arg.dtext }} &gt;
+            </span>
+        </div>
+        <div class="col-6 mx-auto h5" v-if="arg.data">
+            <table class="table table-borderless col-6">
+                <tbody>
+                    <tr v-for="data in arg.data" :key="data">
+                        <th>{{ data.item_name }}</th>
+                        <td class="text-center">{{ data.item }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="text-center my-3 h4" v-else>
+            データがありません
+        </div>
     </div>
 </template>
 
