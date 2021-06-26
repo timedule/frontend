822835fc6ddc5a6c47f2da128cb79e4e4000b423
Laddge <dev.laddge@gmail.com>
2021-06-26 04:46:32 +0000

    [feat] ナビバーにログインボタンを設置

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
     <div>
         <b-navbar type="dark" variant="dark">
-            <b-navbar-brand class="px-3" href="/">
-                <img src="@/assets/logo.png" alt="Logo" height="30px" class="d-inline-block align-top">
-                Timedule
-            </b-navbar-brand>
+            <div class="row w-100">
+                <b-navbar-brand class="col my-auto mx-3" href="/">
+                    <img src="@/assets/logo.png" alt="Logo" height="30px" class="d-inline-block align-top">
+                    Timedule
+                </b-navbar-brand>
+                <div class="col-auto my-auto px-0">
+                    <router-link to="/login" class="link-light">
+                        <b-icon-person v-if="user === null" class="h1 my-auto"></b-icon-person>
+                        <div v-else class="my-auto">
+                            <b-icon-person-circle></b-icon-person-circle>
+                            {{ user.uid }}
+                        </div>
+                    </router-link>
+                </div>
+            </div>
         </b-navbar>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
+import firebase from 'firebase'
+
 export default {
     name: 'App',
 
@@ -20,9 +33,11 @@ export default {
     mounted : function(){
         let routeInstance = this.$route;
         this.createTitleDesc(routeInstance);
+        this.user = firebase.auth().currentUser;
     },
 
     data: () => ({
+        user: null,
         drawer: false,
     }),
 
@@ -46,8 +61,9 @@ export default {
     watch: {
         '$route' (routeInstance, from) {
             from;
+            this.user = firebase.auth().currentUser;
             this.createTitleDesc(routeInstance);
-        }
+        },
     },
 };
 </script>
