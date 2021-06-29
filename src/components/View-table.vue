1ef0a4f479729cb27e7fcc2421728af65ee56711
Laddge <dev.laddge@gmail.com>
2021-06-29 02:40:39 +0000

    [fix] 時間割ビューアーでループのキーが重複する可能性があったので、データにidを付けて回避

--- src/components/View-table.vue
+++ src/components/View-table.vue
@@ -8,7 +8,7 @@
         <div class="col-6 mx-auto h5" v-if="arg.data">
             <table class="table table-borderless col-6">
                 <tbody>
-                    <tr v-for="data in arg.data" :key="data">
+                    <tr v-for="data in arg.data" :key="data.id">
                         <th>{{ data.item_name }}</th>
                         <td class="text-center">{{ data.item }}</td>
                     </tr>
